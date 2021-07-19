1d8b1cdd1fd01bbf600a6b40d7e9af7687d99069
mortiz <mortiz@scentregroup.com>
2021-07-19 22:51:05 +0800

    allow all

--- server.js
+++ server.js
@@ -37,14 +37,9 @@ const main = require('./controllers/main')
 const app = express()
 
 // App Middleware
-const whitelist = ['http://localhost:3001']
 const corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
+    callback(null, true)
   }
 }
 app.use(helmet())
